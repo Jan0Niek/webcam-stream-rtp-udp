366dd7407fc8a06a10891e846a0c59376e301c84
Jan_Niek-PC <yannick.eleveld@coderclass.nl>
2026-02-11 18:37:52 +0100

    serial testen in spanningsdeler.py

--- spanningsdeler.py
+++ spanningsdeler.py
@@ -1,3 +1,9 @@
 import serial
-
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.reset_input_buffer()
+time.sleep(10)  # conserve a little power maybe?
+while running:
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)  # moet eigenlijk weg, gaat dan sneller
 # onzin
