b898b79848e8496c12a80cb9b83163543b7717b5
Jan_Niek-PC <yannick.eleveld@coderclass.nl>
2026-02-11 20:26:49 +0100

    rstrip

--- spanningsdeler.py
+++ spanningsdeler.py
@@ -3,5 +3,5 @@ ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 ser.reset_input_buffer()
 while True:
     if ser.in_waiting > 0:
-        line = ser.readline()
+        line = ser.readline().rstrip()
         print(line)  
