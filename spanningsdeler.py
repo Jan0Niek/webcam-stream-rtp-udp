1567b8b873bb7beb3d5507406b1014d217c31d58
Jan_Niek-PC <yannick.eleveld@coderclass.nl>
2026-02-11 19:10:59 +0100

    serial werkt van arduino volgens mij

--- spanningsdeler.py
+++ spanningsdeler.py
@@ -1,9 +1,7 @@
 import serial
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 ser.reset_input_buffer()
-time.sleep(10)  # conserve a little power maybe?
-while running:
+while True:
     if ser.in_waiting > 0:
         line = ser.readline().decode('utf-8').rstrip()
         print(line)  # moet eigenlijk weg, gaat dan sneller
-# onzin
